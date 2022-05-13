6af601034326710369e7304d44347927740404e5
ROCA Philippe <roca.philippe63@gmail.com>
2022-05-13 17:31:59 +0200

    fix bis

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
               exitEventName: 'sal:out', // Exit event name
         }
         },
+        `gatsby-plugin-styled-components`
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
